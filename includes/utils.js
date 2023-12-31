7553a92059caf62aa39389beacf5705623b06323
VINICIUS  TELES DE OLIVEIRA <viniteles@discente.ufg.br>
2023-12-31 12:46:36 +0000

    initial commit

--- includes/utils.js
+++ includes/utils.js
@@ -1,11 +1,18 @@
-function format_description(description) {
+function is_yellow(description) {
     return `CASE
-    WHEN ${description} LIKE '%Yellow%' THEN 'yellow' 
-    WHEN ${description} LIKE '%Red%' THEN 'red'
-    ELSE 'none'
+    WHEN ${description} LIKE '%Yellow%' THEN 1
+    WHEN ${description} LIKE '%yellow%' THEN 1
+    END`;
+}
+
+function is_red(description) {
+    return `CASE
+    WHEN ${description} LIKE '%Red%' THEN 1
+    WHEN ${description} LIKE '%red%' THEN 1
     END`;
 }
 
 module.exports = {
-    format_description
+    is_yellow,
+    is_red
 };
