f13a4e80bf53350a033c86ae843d7c50e75301f6
VINICIUS  TELES DE OLIVEIRA <viniteles@discente.ufg.br>
2023-12-31 15:48:22 +0000

    adding games table to dw

--- includes/utils.js
+++ includes/utils.js
@@ -12,7 +12,24 @@ function is_red(description) {
     END`;
 }
 
+function get_card(type_card, type_club) {
+    return "(SELECT " +
+        String(type_card) + "_cards " +
+        "FROM football_dw_inf.load_cards lc WHERE (g.game_id = lc.game_id AND g." +
+        String(type_club) +
+        "_club_id = lc.club_id))"
+}
+
+function mapping_cards_to_game(type_card, type_club) {
+    return "CASE WHEN " +
+        String(get_card(type_card, type_club)) +
+        " IS NULL THEN 0" +
+        " ELSE " + String(get_card(type_card, type_club)) +
+        " END"
+}
+
 module.exports = {
     is_yellow,
-    is_red
+    is_red,
+    mapping_cards_to_game
 };
